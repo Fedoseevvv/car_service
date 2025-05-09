b94c28e205f0f4d24e313d83ed78e133b9df1289
Fedoseevvv <danila.fedoseev.1997@yandex.ru>
2025-05-09 20:04:37 +0300

    Fix Node.js modules in client-side code

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    ppr: false
-  }
-}
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        perf_hooks: false,
+      };
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig; 
